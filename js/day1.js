334e8c9b3edb0bc4a0b485d8a0884629835722cf
Cristian Mendoza <thattechnokid@gmail.com>
2018-01-18 17:51:32 -0800

    Takehome

--- js/day1.js
+++ js/day1.js
@@ -1,2 +1,3 @@
 
 // ----  Added Emmet, atom-live-server, open-in-browser, & Platformio IDE Terminal to ATOM  ----
+console.log("What i would like to get out of this class is a good understanding of JavaScript, its purpose, and what I can do to show my talents to the world. Haha. a");
