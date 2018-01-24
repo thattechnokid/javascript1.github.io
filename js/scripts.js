a7defcdbe247388476b096622923b280f79ef7de
Cristian Mendoza <thattechnokid@gmail.com>
2018-01-23 20:12:56 -0800

    Comparison operators, etc

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,25 @@
+ // -------  Logical Operators  ----------
 
-// ----  Added Emmet, atom-live-server, open-in-browser, & Platformio IDE Terminal to ATOM  ----
+ const bestNumber = 42;
+
+ console.log(!bestNumber); //Checks if this even exists
+
+ // ------  Comparison Operators  ------------
+
+ var string = "1";
+ var number = 1;
+ console.log(string == number); //Expects true: same value...loosely equal
+
+ console.log(string === number); //Expects false: same value, DIFFERENT type ... strictly not equal
+
+  // -------  Practice  -----------
+
+  var string1 = "Hello Bro";
+  var string2 = new String();
+  var string3 = new String("Hello Bro");
+
+  console.log(string1 === string2); //Returns false
+  console.log(string1 == string2); //Returns false
+
+  console.log(string1 === string3); //Returns false
+  console.log(string1 == string3); //Returns true
