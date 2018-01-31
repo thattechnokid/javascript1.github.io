05cbdff795e99661766415c3192f371f0df384b8
Cristian Mendoza <thattechnokid@gmail.com>
2018-01-30 20:38:57 -0800

    Learned about loops | End of day4

--- js/scripts.js
+++ js/scripts.js
@@ -59,3 +59,139 @@ console.log(myCopiedArray);
 
 myCopiedArray = myArray.slice(1, 6); //copies the array from the 1st index and copies all the way to the end. if there are 5 indexes, to get the last element, put 6 in the second parameter of the slice function
 console.log(myCopiedArray);
+
+
+// -------------------  Take home day 3  -------------------------
+
+// // var favColor = prompt("What is your favorite color?", "blue or green");
+//
+// // favColor = favColor.toLowerCase();
+//
+// switch (favColor.toLowerCase()) {
+//   case "blue":
+//     alert("Are you feeling blue?");
+//     break;
+//   case "green":
+//     alert("The greener the better");
+//     break;
+//   default:
+//     alert("You're boring... Enter a valid color");
+//
+// }
+
+// -----------------  String Interpolation  ---------------
+//
+// var firstName = prompt("Enter Your First Name", "John");
+// var lastName = prompt("Enter Your Last Name", "Doe");
+// var color = prompt("Whats Your favorite color?");
+
+//New way of doing this:
+
+// alert(`Hey there ${firstName}! Your favorite color is ${color}, and your Last name is ${lastName}`);
+
+
+// --------------  Find index of an item in array  -------------------
+var arrayOne = [1, 2, 3, 4, 5];
+
+if (arrayOne.indexOf(6) !== -1) {
+  alert("It is there!!");
+} else {
+  // alert("It is not in the array");
+  console.log(arrayOne.indexOf(6));
+}
+
+// -------------  reverse items in an array  --------------
+console.log("---- Before ----");
+console.log(arrayOne);
+console.log("---- After ----");
+console.log(arrayOne.reverse());
+
+
+// -----------  Sorting arrays' elements  ------------
+
+var days = [2, 1, 6, 31, 88, 63];
+console.log("---- Before ----");
+console.log(days);
+console.log("---- After ----");
+console.log(days.sort());
+
+
+
+// --------------  Multi-Dimensional Arrays  -----------------
+
+var favoriteMovie = ['Star Wars VI', '1983'];
+    var secondFavorite = ['Return of the King', '2003'];
+    var thirdFavorite = ['Equilibrium', '2002'];
+    var myFavoriteMovies = [];
+    myFavoriteMovies.push(favoriteMovie, secondFavorite, thirdFavorite);
+    //Result:
+    [['Star Wars VI', '1983'], ['Return of the King', '2003'], ['Equilibrium', '2002']];
+
+var movieYears = [];
+movieYears.push(myFavoriteMovies[0][1], myFavoriteMovies[1][1],myFavoriteMovies[2][1]);
+// alert(movieYears);
+
+// ------------  PRACTICE  ---------------
+
+var firstMovie = ["Star Wars 8", "Rey"];
+var secondMovie = ["The Greatest Showman", "P.T. Barnum"];
+var thirdMovie = ["Batman", "Batman"];
+var fourthMovie = ["Superman", "Clark Kent"];
+var fifthMovie = ["Maze Runner", "Thomas"];
+
+var movieTitles = [];
+var favorites = [];
+var leastFavorites = [];
+
+movieTitles.push(firstMovie, secondMovie, thirdMovie, fourthMovie, fifthMovie);
+// alert(`Your favorite movies are: ${movieTitles}`);
+favorites.push(movieTitles[0]);
+leastFavorites.push(movieTitles[2]);
+console.log(favorites);
+console.log(leastFavorites.length);
+
+
+
+// ------------------  LOOPS  ---------------
+
+// ----  While Loop  ----
+
+var n = 0;
+while (n < 3) {
+  n++;
+}
+console.log(n);
+
+// --------------------------------------------
+var i = 0;
+while (i < movieTitles.length) {
+  console.log(movieTitles[i]);
+  // NOTE: Without i++, it will be an infinite loop!!
+  i++;
+}
+
+// ---- FOR Loop ----
+for (var i = 0; i < movieTitles.length; i++) {
+console.log(movieTitles[i]);
+}
+
+// ----  DO While Loop  ----
+var result = '';
+var i = 0;
+
+do {
+  i += 1;
+
+  result += i;
+} while (i < 5);
+
+console.log(result); //Outputs 12345
+
+// ---------------  PRACTICE  -----------------
+for (var i = 0; i < movieTitles.length; i++) {
+  if (movieTitles[i][0] === "The Greatest Showman") {
+    // alert(`Your Favorite movie is ${movieTitles[i][0]} with ${movieTitles[i][1]} as the main character`);
+  }
+}
+
+//ENd
