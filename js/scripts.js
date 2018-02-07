00cfcd4fe5706746d2791495ece6622b847ee46a
Cristian Mendoza <thattechnokid@gmail.com>
2018-02-06 18:27:55 -0800

    Reviewed take-home | end of day5

--- js/scripts.js
+++ js/scripts.js
@@ -34,26 +34,86 @@ console.log("---------------------------");
 myMagicalPony("My girlfriend", "yours"); //Now it will actually work and make sense.
 
 
-// --------------------------  Arrow Functions  -------------------------------
-
-
-
-
-
-
 
 
 //---------- Practice -----------
 
-function dogYears(yearsOld){
-  if (yearsOld.typeOf !== "Number") {
-    alert("You must enter a number like this: 4");
-  } else {
-    var dogsAge = yearsOld * 7;
-    alert(`Your dog is ${dogsAge} years old in "Dog Years"`);
-  }
+// function dogYears(yearsOld){
+//   console.log("User inputted: ",yearsOld, "type: ",typeof yearsOld);
+//
+//   if (isNaN(yearsOld)) {
+//      alert("You must enter a number like this: 4");
+//
+//   } else if (typeof yearsOld == 'number') {
+//     alert(`Your dog is ${yearsOld * 7} years old in "Dog Years"`);
+//
+//   }
+//
+// }
+//
+// dogYears(parseInt(prompt("How Old Is Your Dog?")));
 
-}
-dogYears(prompt("How Old Is Your Dog?"));
 
 // --------- Practice 2 --------
+
+// function lifeTimeSupply(userAge, prodAmount, maxAge){
+//   if (isNaN(userAge) || isNaN(prodAmount) || isNaN(maxAge)) {
+//     alert("You Must enter a valid number");
+//   } else {
+//     prodAmount *= 365;
+//     var difference = maxAge - userAge;
+//     alert(`You will need ${prodAmount * difference} of your product to last you until you are ${maxAge} years old`);
+//   }
+// }
+//
+// lifeTimeSupply(parseInt(prompt("Enter Your Age: ")), parseInt(prompt("How Much of the product do you use/consume per day?", "Enter a Number: 1")), parseInt(prompt("Enter the age you want the product to last you until:", "i.e. 80")));
+
+
+// ------------------------  Take Home  ----------------------------
+
+//-----  Part ONE  -----
+  // function squareOf(num){
+  //   if (isNaN(num)) {
+  //     alert("You must enter a valid number like so: 3");
+  //     squareOf(parseInt(prompt("Enter the number to raise to the power of 2")));
+  //
+  //   } else {
+  //     console.log(Math.pow(num, 2));
+  //
+  //   }
+  //
+  // }
+  // squareOf(parseInt(prompt("Enter the number to raise to the power of 2")));
+
+
+//-----  Part TWO  -----
+// function properGrammar(sentence){
+//   // NOTE: -> This can be outside the alert, but basically, it capitalizes the first letter of the sentence, and then adds the rest of the sentence using the sice() method starting with the letter after the first letter
+//   if (sentence.endsWith('.')) {
+//     alert(`${sentence.charAt(0).toUpperCase() + sentence.slice(1)}`);
+//   }else {
+//     alert(`${sentence.charAt(0).toUpperCase() + sentence.slice(1) + "."}`);
+//
+//   }
+// }
+//
+// properGrammar(prompt("Enter a sentence here all lowercase: ","here at the wall"));
+
+
+//-----  Part THREE  -----
+// function stringFlipper(flipMe){
+//   alert(`${flipMe.substr(-(flipMe.length / 2)) + flipMe.substr(0,(flipMe.length / 2))}`);
+// }
+// stringFlipper(prompt("Enter a sentence or word to flip"));
+
+//-----  Part FOUR Bonus  -----
+function isPalindrome(s){
+  var reversed = s.split('').reverse().join(''); //the '' makes it split each individual letter into an array
+  if (s === reversed) {
+    alert("Its a palindrome!");
+    return true;
+  }else {
+    alert('Not a palindrome');
+    return false;
+  }
+}
